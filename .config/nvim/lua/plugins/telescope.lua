f2775f5bbcabaf1c4d1f0183d1a6b3a5b37f6052
endalk200 <eb808826@gmail.com>
2024-07-13 20:58:00 +0300

    config: updated keymap for telescop git file and find files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -12,13 +12,13 @@ return {
     })
     local builtin = require("telescope.builtin")
 
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+    vim.keymap.set("n", "<leader>ff", builtin.git_files, {})
     vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
     vim.keymap.set("n", "<leader>fr", builtin.registers, {})
     vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {})
-    vim.keymap.set("n", "<leader><leader>", builtin.git_files, {})
+    vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
     vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
     vim.keymap.set("n", "<leader>ss", builtin.lsp_document_symbols, {})
   end,
